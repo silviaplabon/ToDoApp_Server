4c8646e93bd0558bdb88a836f1863b1d76b1dba4
silviaplabon <silviaplabon@gmail.com>
2021-08-03 14:52:12 +0600

    insert todo

--- index.js
+++ index.js
@@ -13,23 +13,30 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mcsxh.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const ToDoCollection = client.db('ToDoList').collection("ToDoCollection");
-  
-  app.get('/all_ToDo_List', (req, res) => {
-   ToDoCollection.find({})
-      .toArray((err, products) => {
-        res.send(products)
-      })
-  })
-  app.delete('/deleteProduct/PopularIngredients/:id', (req, res) => {
-    PopularIngredientsCollection.deleteOne({ _id: ObjectID(req.params.id) })
-      .then(result => {
-        res.send(result.deletedCount > 0)
-      })
-  })
+    const ToDoCollection = client.db('ToDoList').collection("ToDoCollection");
+
+    app.get('/all_ToDo_List', (req, res) => {
+        ToDoCollection.find({})
+            .toArray((err, products) => {
+                res.send(products)
+            })
+    })
+    app.post('/add_To-Do', (req, res) => {
+        const newToDo = req.body;
+        ToDoCollection.insertOne(newToDo)
+            .then(result => {
+                res.send(result.insertedCount > 0)
+            })
+    })
+    app.delete('/deleteProduct/PopularIngredients/:id', (req, res) => {
+        PopularIngredientsCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
+                res.send(result.deletedCount > 0)
+            })
+    })
 });
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
 
 
